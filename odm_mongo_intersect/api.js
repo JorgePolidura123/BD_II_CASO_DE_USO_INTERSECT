c224769283e3f7745eb0febeb77b5f4fd3c39f91
martin <martin.lopez@alumnos.uneatlantico.es>
2025-03-06 15:11:48 +0100

    correccion

--- odm_mongo_intersect/api.js
+++ odm_mongo_intersect/api.js
@@ -1,16 +1,18 @@
-const API = 'http://localhost:8000';
 
-async function getdata() {
-  const fetch = (await import('node-fetch')).default; 
-  
-  try {
-    const res = await fetch(API);
-    const data = await res.json();
-    return data;
-  } catch (e) {
-    console.log('❌ Error:', e);
-    process.exit(1);
-  }
+import fetch from 'node-fetch';//este es for ESM module newer
+//const fetch = require('node-fetch')//CommonJS older
+//const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));// solución for both compatibility
+let API = 'http://localhost:8000';
+
+export async function getdata() {
+const res = await fetch(API);
+const data = await res.json();
+try {
+ //console.log(data);
+ return data;
+} catch (e) {
+ console.log('Some error');
+ console.log(e);
+ process.exit(0);
 }
-
-module.exports = { getdata };
+}
